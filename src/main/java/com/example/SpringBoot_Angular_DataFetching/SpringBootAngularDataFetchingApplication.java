9b4496c3aaf45225436fe684d114237c5f797098
Njoura7 <njourawebdev@gmail.com>
2024-11-19 01:15:12 +0100

    first commit

---
+++ src/main/java/com/example/SpringBoot_Angular_DataFetching/SpringBootAngularDataFetchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Angular_DataFetching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularDataFetchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularDataFetchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_Angular_DataFetching/SpringBootAngularDataFetchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Angular_DataFetching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularDataFetchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
